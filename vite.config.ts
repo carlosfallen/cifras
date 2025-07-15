ba5c3ba0aa4ceb19c55163c4e406ce259a0d0041
Carlos <61169872+carlosfallen@users.noreply.github.com>
2025-07-15 11:47:16 -0300

    Fix Git merge conflict in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   server: {
-    host: '0.0.0.0', // <-- aqui!
-    port: 5173, // opcional, defina se quiser uma porta fixa
+    host: '0.0.0.0',
+    port: 5173,
   },
-=======
->>>>>>> 6dce138b82ee42ab581451b7550730ca0522ac21
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
